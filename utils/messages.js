0b4f11ba261fa5b4352e48cef3ade26afee86179
Türker <63150613+turkerpw@users.noreply.github.com>
2022-04-23 19:05:27 +0300

    Add files via upload

---
+++ utils/messages.js
@@ -0,0 +1,15 @@
+const config = require('../config.json');
+
+module.exports = {
+    giveaway: "🎉🎉 **ÇEKİLİŞ BAŞLADI** 🎉🎉",
+    giveawayEnded: "🎉🎉 **ÇEKİLİŞ SONLANDI** 🎉🎉",
+    inviteToParticipate: "🎉 Emojisine basarak katılabilrisin!",
+    dropMessage: "🎉 Emojiye ilk basan kazanır!",
+    drawing: 'Kalan: {timestamp}',
+    winMessage: "Tebrikler!!, {winners}! **{this.prize}** Kazandın!",
+    embedFooter: "Türker",
+    noWinner: "Çekiliş iptal edildi kazanan yok.",
+    hostedBy: "Sponsor: {this.hostedBy}",
+    winners: "kazanan(lar)",
+    endedAt: "Sonlandı"
+};
